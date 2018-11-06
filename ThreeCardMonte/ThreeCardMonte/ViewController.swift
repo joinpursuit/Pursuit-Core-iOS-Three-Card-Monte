e066f744a7c450757daedb22a2b176fc7bc95633
BironSu <bironsu@pursuit.org>
2018-11-06 16:21:15 -0500

    Hard coding to get result to test

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -10,11 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstCard: UIButton!
+    @IBOutlet weak var secondCard: UIButton!
+    @IBOutlet weak var thirdCard: UIButton!
+    @IBOutlet weak var newGame: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+    @IBAction func cardSelect (_ sender: UIButton) {
+//        var cardReveal: (pickCard: UIImage?, choice: String)?
+        let randArray: [Int] = [0,1,2]
+        var randomEle = randArray.randomElement()
+        switch sender.tag {
+            case 0:
+                if sender.tag == randomEle {
+                    firstCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                    secondCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    thirdCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    messageLabel.text = "You found the king!"
+                } else {
+                    firstCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                    secondCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                    thirdCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    messageLabel.text = "Wrong card!"
+                }
+                firstCard.isEnabled = false
+                secondCard.isEnabled = false
+                thirdCard.isEnabled = false
+            case 1:
+                if sender.tag == randomEle {
+                    firstCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    secondCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                    thirdCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    messageLabel.text = "You found the king!"
+                } else {
+                    firstCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                    secondCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                    thirdCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    messageLabel.text = "Wrong card!"
+                }
+                firstCard.isEnabled = false
+                secondCard.isEnabled = false
+                thirdCard.isEnabled = false
+            case 2:
+                if sender.tag == randomEle {
+                    firstCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    secondCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    thirdCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                    messageLabel.text = "You found the king!"
+                } else {
+                    firstCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                    secondCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                    thirdCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                    messageLabel.text = "Wrong card!"
+                }
+                firstCard.isEnabled = false
+                secondCard.isEnabled = false
+                thirdCard.isEnabled = false
+            case 3:
+                firstCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                secondCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                thirdCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+                randomEle = randArray.randomElement()
+                firstCard.isEnabled = true
+                secondCard.isEnabled = true
+                thirdCard.isEnabled = true
+                messageLabel.text = "Find the king!"
+            default:
+                print("error")
+        }
+    }
+
 
 }
+
 
