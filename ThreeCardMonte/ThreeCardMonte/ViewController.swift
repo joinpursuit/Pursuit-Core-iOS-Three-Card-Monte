a2e2701dba1b790119afae239371e5885c73f96a
natmess <nnmessier@gmail.com>
2018-11-06 17:16:39 -0500

    added cards

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+//outlets
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-
+    @IBAction func flipCards(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+          firstButton.setImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
+        case 1:
+            
+        case 2:
+            
+        default:
+        }
+    }
 }
 
