5b2ffc6ab4773ac3935eb96b626776a1ace7e774
JianTing-Li <jiantingli@pursuit.org>
2018-11-06 23:19:49 -0500

    Working Game, need to refractor code

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+    //outlets
+    @IBOutlet weak var card1: UIButton!
+    @IBOutlet weak var card2: UIButton!
+    @IBOutlet weak var card3: UIButton!
+    @IBOutlet weak var gameState: UILabel!
+    @IBOutlet weak var newGame: UIButton!
+    
+    var cards = [UIButton]()
+    
+    var images = [UIImage.init(named: "threeCard"),
+                  UIImage.init(named: "threeCard"),
+                  UIImage.init(named: "kingCard")]
+    
+    let faceDownCard = UIImage.init(named: "cardBackRed")
+    
+    override func viewDidLoad() { //runs 1 time at the beginning
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    cards = [card1, card2, card3]
+    images = images.shuffled()
+    }
+  
+    //actions
+    @IBAction func flipCard(_ sender: UIButton) {
+        //switch on the tag
+          //0, 1, or 2
+        switch sender.tag {
+        case 0:
+            card1.setImage(images[0], for: .normal)
+            if images[0] == UIImage.init(named: "kingCard") {
+                gameState.text = "You Win! 😄"
+            } else {
+                gameState.text = "You Lose 😢. Better Luck next time 😉."
+            }
+        case 1:
+            card2.setImage(images[1], for: .normal)
+            if images[1] == UIImage.init(named: "kingCard") {
+                gameState.text = "You Win! 😄"
+            } else {
+                gameState.text = "You Lose 😢. Better Luck next time 😉."
+            }
+        case 2:
+            card2.setImage(images[2], for: .normal)
+            if images[2] == UIImage.init(named: "kingCard") {
+                gameState.text = "You Win! 😄"
+            } else {
+                gameState.text = "You Lose 😢. Better Luck next time 😉."
+            }
+        default:
+            print("Error")
+        }
 
-
+    }
+    
+    //new action for reset
+    @IBAction func resetGame(_ sender: UIButton) {
+        images = images.shuffled()
+        gameState.text = "Pick A Card 🤚🏽"
+        card1.setImage(faceDownCard, for: .normal)
+        card2.setImage(faceDownCard, for: .normal)
+        card3.setImage(faceDownCard, for: .normal)
+    }
 }
 
