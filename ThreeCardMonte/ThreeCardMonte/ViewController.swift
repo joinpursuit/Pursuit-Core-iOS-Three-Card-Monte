3fa58aae22980b8d78032c03f9a7dad1f2b253a5
josealarconchacon <jose.alarcon.chacom@gmail.com>
2018-11-06 21:50:35 -0500

    update

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+    
+    @IBOutlet weak var firstC: UIButton!
+    @IBOutlet weak var SECONC: UIButton!
+    @IBOutlet weak var thedC: UIButton!
+    
+    @IBOutlet weak var outletCard: UILabel!
+    
+    
+    
+  
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+    
+    @IBAction func cards(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 0:
+            sender.setImage(UIImage.init(named: "cardBackRed"),for: .normal)
+            outletCard.text = "You lost"
+            firstC.isEnabled = false
+            firstC.isEnabled = false
+        case 1:
+            sender.setImage(UIImage.init(named: "kingCard"),for: .normal)
+            outletCard.text = "You wont"
+            SECONC.isEnabled = false
+            SECONC.isEnabled = false
+
+            
+        case 2:
+            sender.setImage(UIImage.init(named: "threeCard"),for: .normal)
+            outletCard.text = "You lost"
+            thedC.isEnabled = false
+            thedC.isEnabled = false
+
+           
+        default:
+            print("Invalid input")
+        }
+   }
 
 }
-
