354ff8b485c492e19332b2d8b0d92f262c983a22
JianTing-Li <jiantingli@pursuit.org>
2018-11-07 00:08:51 -0500

    refractored code now go for the bonus

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     
     var cards = [UIButton]()
     
+    //card images when flipped
+        //how to safely unwrap this
     var images = [UIImage.init(named: "threeCard"),
                   UIImage.init(named: "threeCard"),
                   UIImage.init(named: "kingCard")]
@@ -32,43 +34,69 @@ class ViewController: UIViewController {
   
     //actions
     @IBAction func flipCard(_ sender: UIButton) {
-        //switch on the tag
-          //0, 1, or 2
+        var winOrLose: Bool = true
         switch sender.tag {
         case 0:
             card1.setImage(images[0], for: .normal)
-            if images[0] == UIImage.init(named: "kingCard") {
-                gameState.text = "You Win! 😄"
-            } else {
-                gameState.text = "You Lose 😢. Better Luck next time 😉."
-            }
+            winOrLose = winOrLoseFunc(imageOptional: images[0], gameState: gameState)
+            if !winOrLose { flipKingCard(cards: cards, images: images) }
         case 1:
             card2.setImage(images[1], for: .normal)
-            if images[1] == UIImage.init(named: "kingCard") {
-                gameState.text = "You Win! 😄"
-            } else {
-                gameState.text = "You Lose 😢. Better Luck next time 😉."
-            }
+            winOrLose = winOrLoseFunc(imageOptional: images[1], gameState: gameState)
+            if !winOrLose { flipKingCard(cards: cards, images: images) }
         case 2:
-            card2.setImage(images[2], for: .normal)
-            if images[2] == UIImage.init(named: "kingCard") {
-                gameState.text = "You Win! 😄"
-            } else {
-                gameState.text = "You Lose 😢. Better Luck next time 😉."
-            }
+            card3.setImage(images[2], for: .normal)
+            winOrLose = winOrLoseFunc(imageOptional: images[2], gameState: gameState)
+            if !winOrLose { flipKingCard(cards: cards, images: images) }
         default:
             print("Error")
         }
-
+        
+        buttonControlOnOff(buttons: cards, enabled: false)
     }
     
-    //new action for reset
+    //Reset Game When "New Game" is tapped
     @IBAction func resetGame(_ sender: UIButton) {
         images = images.shuffled()
         gameState.text = "Pick A Card 🤚🏽"
         card1.setImage(faceDownCard, for: .normal)
         card2.setImage(faceDownCard, for: .normal)
         card3.setImage(faceDownCard, for: .normal)
+        buttonControlOnOff(buttons: cards, enabled: true)
+    }
+    
+    
+    
+    //Functions
+    func buttonControlOnOff(buttons: [UIButton], enabled: Bool) -> () {
+        for button in buttons {
+            if enabled {
+                button.isEnabled = true
+                continue
+            }
+            button.isEnabled = false
+        }
+    }
+    
+
+    func winOrLoseFunc(imageOptional: UIImage?, gameState: UILabel) -> Bool {
+        guard let image = imageOptional else {return false}
+        if image == UIImage.init(named: "kingCard") {
+            gameState.text = "You Win! 😄"
+            return true
+        } else {
+            gameState.text = "You Lose 😢. Better Luck next time 😉."
+            return false
+        }
+    }
+    
+    func flipKingCard(cards: [UIButton], images: [UIImage?]) -> () {
+        for (i, imageOptional) in images.enumerated() {
+            guard let image = imageOptional else {continue}
+            if image == UIImage.init(named: "kingCard") {
+                cards[i].setImage(image, for: .normal)
+            }
+        }
     }
 }
 
