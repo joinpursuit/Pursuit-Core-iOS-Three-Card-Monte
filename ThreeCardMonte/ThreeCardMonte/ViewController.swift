bad117239e88e9d80493f3eeb6221313667be874
natmess <nnmessier@gmail.com>
2018-11-07 12:54:55 -0500

    finished project

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class ViewController: UIViewController {
    
+    @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var card1: UIButton!
     @IBOutlet weak var card2: UIButton!
     @IBOutlet weak var card3: UIButton!
@@ -25,41 +26,48 @@ class ViewController: UIViewController {
         
         logoImage = logoImage.shuffled()
         
-        
-        
-        
         switch sender.tag {
         case 0:
             if logoImage[0] == UIImage(named: "kingCard")! {
                 card1.setImage(UIImage.init(named: "kingCard"), for: .normal)
-                card1.isEnabled = false
-                //change label to you win
-                //disable
-                //break
+                displayLabel.text = "you win!"
                 
             } else {
                 card1.setImage(UIImage.init(named: "threeCard"), for: .normal)
-                //change label to oyu lost
-                //flip where the king is
-                //disable
+                displayLabel.text = "you lose!"
                 
             }
+            card1.isEnabled = false
+            card2.isEnabled = false
+            card3.isEnabled = false
         case 1:
             if logoImage[1] == UIImage(named: "kingCard")! {
                 card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                displayLabel.text = "you win!"
             } else {
                 card2.setImage(UIImage.init(named: "threeCard"), for: .normal)
+            displayLabel.text = "you lose!"
             }
+            card1.isEnabled = false
+            card2.isEnabled = false
+            card3.isEnabled = false
         case 2:
             if logoImage[2] == UIImage(named: "kingCard")! {
                 card3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                displayLabel.text = "you win!"
             } else {
                 card3.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                displayLabel.text = "you lose!"
             }
+            card1.isEnabled = false
+            card2.isEnabled = false
+            card3.isEnabled = false
         default:
             print("INVALID TAG")
         }
-        
+        card1.isEnabled = false
+        card2.isEnabled = false
+        card3.isEnabled = false
         
     }
     
@@ -71,6 +79,18 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func newGame(_ sender: Any) {
+        
+        displayLabel.text = "Pick a Card"
+        
+        card1.isEnabled = true
+        card2.isEnabled = true
+        card3.isEnabled = true
+        
+        card1.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+         card2.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+         card3.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+    }
 }
 
 
