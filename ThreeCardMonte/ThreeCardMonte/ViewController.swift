e1c97b2593fa0956ef6e123412dbd50da9b05c40
BironSu <bironsu@pursuit.org>
2018-11-07 16:22:34 -0500

    reduced line to 115 thanks alex

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-var randArray: [Int] = [0,1,2,3,4]
-var randomEle = randArray.randomElement()
+var randNum = 0..<2
+var randomEle = Int.random(in: randNum)
 var correctGuess = 0
 var wrongGuess = 0
 class ViewController: UIViewController {
-
-    
     @IBOutlet weak var firstCard: UIButton!
     @IBOutlet weak var secondCard: UIButton!
     @IBOutlet weak var thirdCard: UIButton!
@@ -34,39 +32,22 @@ class ViewController: UIViewController {
     setHide()
     gameTitle.font = UIFont(name: "HelveticaNeue-UltraLight", size: 80.0)
     gameTitle.text = "Three Card Monte!!"
-    
-    // Do any additional setup after loading the view, typically from a nib.
   }
     func cardReveal(a: Int, b: Int) {
-        switch a {
-        case 0 :
-            firstCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-        case 1 :
-            secondCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-        case 2 :
-            thirdCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-        case 3 :
-            fourthCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-        case 4 :
-            fifthCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-        default :
-            print("Something went wrong")
+        var array = [firstCard!,secondCard!,thirdCard!,fourthCard!,fifthCard!]
+        if a != b {
+            array[a].setImage(UIImage.init(named: "kingCard"), for: .normal)
+            array[b].setImage(UIImage.init(named: "threeCard"), for: .normal)
+            messageLabel.text = "Wrong Card!"
+            wrongGuess += 1
+        } else {
+            array[a].setImage(UIImage.init(named: "kingCard"), for: .normal)
+            messageLabel.text = "You found the king!"
+            correctGuess += 1
         }
-        switch b {
-        case 0 :
-            firstCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
-        case 1 :
-            secondCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
-        case 2 :
-            thirdCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
-        case 3 :
-            fourthCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
-        case 4 :
-            fifthCard.setImage(UIImage.init(named: "threeCard"), for: .normal)
-        default :
-            print("Something went wrong")
-        }
-
+        correctLabel.text = String(correctGuess)
+        wrongLabel.text = String(wrongGuess)
+        setFalse()
     }
     func setFalse() {
         firstCard.isEnabled = false
@@ -83,166 +64,42 @@ class ViewController: UIViewController {
         fifthCard.isHidden = true
     }
     func newGameCard(a: Int) {
-        switch a {
-        case 3:
-            randArray = [0,1,2]
-            firstCard.isHidden = false
-            secondCard.isHidden = false
-            thirdCard.isHidden = false
-            firstCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            secondCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            thirdCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            fourthCard.isHidden = true
-            fifthCard.isHidden = true
-            randomEle = randArray.randomElement()
-            firstCard.isEnabled = true
-            secondCard.isEnabled = true
-            thirdCard.isEnabled = true
-            fourthCard.isEnabled = false
-            fifthCard.isEnabled = false
-        case 4:
-            randArray = [0,1,2,3]
-            firstCard.isHidden = false
-            secondCard.isHidden = false
-            thirdCard.isHidden = false
-            fourthCard.isHidden = false
-            firstCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            secondCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            thirdCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            fourthCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            fifthCard.isHidden = true
-            randomEle = randArray.randomElement()
-            firstCard.isEnabled = true
-            secondCard.isEnabled = true
-            thirdCard.isEnabled = true
-            fourthCard.isEnabled = true
-            fifthCard.isEnabled = false
-        case 5:
-            randArray = [0,1,2,3,4]
-            firstCard.isHidden = false
-            secondCard.isHidden = false
-            thirdCard.isHidden = false
-            fourthCard.isHidden = false
-            fifthCard.isHidden = false
-            firstCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            secondCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            thirdCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            fourthCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            fifthCard.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
-            randomEle = randArray.randomElement()
-            firstCard.isEnabled = true
-            secondCard.isEnabled = true
-            thirdCard.isEnabled = true
-            fourthCard.isEnabled = true
-            fifthCard.isEnabled = true
-        default:
-            print("Something went wrong in newGameCard function")
+        setFalse()
+        setHide()
+        gameTitle.isHidden = true
+        var array = [firstCard!,secondCard!,thirdCard!,fourthCard!,fifthCard!]
+        randNum = 0..<a
+        randomEle = Int.random(in: randNum)
+        for i in 0..<a {
+            array[i].setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+            array[i].isHidden = false
+            array[i].isEnabled = true
+        }
+        messageLabel.text = "Find the king!"
+        titleChanger(a: a)
+    }
+    func titleChanger(a: Int) {
+        let array = [3,4,5]
+        let ngArray = [newGame3!,newGame4!,newGame5!]
+        newGame3.setTitle("New Game - 3 Cards", for: .normal)
+        newGame4.setTitle("New Game - 4 Cards", for: .normal)
+        newGame5.setTitle("New Game - 5 Cards", for: .normal)
+        if array.contains(a) {
+            let num = a - 3
+            ngArray[num].setTitle("Continue", for: .normal)
         }
     }
     @IBAction func cardSelect (_ sender: UIButton) {
-        switch sender.tag {
-            case 0:
-                if sender.tag == randomEle {
-                    firstCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-                    messageLabel.text = "You found the king!"
-                    correctGuess += 1
-                } else {
-                    if let randomInt = randomEle {
-                        cardReveal(a: randomInt, b: sender.tag)
-                    }
-                    wrongGuess += 1
-                    messageLabel.text = "Wrong card!"
-                }
-                correctLabel.text = String(correctGuess)
-                wrongLabel.text = String(wrongGuess)
-                setFalse()
-            case 1:
-                if sender.tag == randomEle {
-                    secondCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-                    messageLabel.text = "You found the king!"
-                    correctGuess += 1
-                } else {
-                    if let randomInt = randomEle {
-                        cardReveal(a: randomInt, b: sender.tag)
-                    }
-                    wrongGuess += 1
-                    messageLabel.text = "Wrong card!"
-                }
-                correctLabel.text = String(correctGuess)
-                wrongLabel.text = String(wrongGuess)
-                setFalse()
-            case 2:
-                if sender.tag == randomEle {
-                    thirdCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-                    messageLabel.text = "You found the king!"
-                    correctGuess += 1
-                } else {
-                    if let randomInt = randomEle {
-                        cardReveal(a: randomInt, b: sender.tag)
-                    }
-                    wrongGuess += 1
-                    messageLabel.text = "Wrong card!"
-                }
-                correctLabel.text = String(correctGuess)
-                wrongLabel.text = String(wrongGuess)
-                setFalse()
-            case 3:
-                if sender.tag == randomEle {
-                    fourthCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-                    messageLabel.text = "You found the king!"
-                    correctGuess += 1
-                } else {
-                    if let randomInt = randomEle {
-                        cardReveal(a: randomInt, b: sender.tag)
-                    }
-                    wrongGuess += 1
-                    messageLabel.text = "Wrong card!"
-                }
-                correctLabel.text = String(correctGuess)
-                wrongLabel.text = String(wrongGuess)
-                setFalse()
-            case 4:
-                if sender.tag == randomEle {
-                    fifthCard.setImage(UIImage.init(named: "kingCard"), for: .normal)
-                    messageLabel.text = "You found the king!"
-                    correctGuess += 1
-                } else {
-                    if let randomInt = randomEle {
-                        cardReveal(a: randomInt, b: sender.tag)
-                    }
-                    wrongGuess += 1
-                    messageLabel.text = "Wrong card!"
-                }
-                correctLabel.text = String(correctGuess)
-                wrongLabel.text = String(wrongGuess)
-                setFalse()
-            default:
-                print("error")
-        }
+        cardReveal(a: randomEle, b: sender.tag)
     }
     @IBAction func newGame3 (_ sender: UIButton) {
-        gameTitle.isHidden = true
         newGameCard(a: 3)
-        messageLabel.text = "Find the king!"
-        sender.setTitle("Continue!", for: .normal)
-        newGame4.setTitle("New Game - 4 Cards", for: .normal)
-        newGame5.setTitle("New Game - 5 Cards", for: .normal)
     }
     @IBAction func newGame4 (_ sender: UIButton) {
-        gameTitle.isHidden = true
         newGameCard(a: 4)
-        messageLabel.text = "Find the king!"
-        sender.setTitle("Continue!", for: .normal)
-        newGame3.setTitle("New Game - 3 Cards", for: .normal)
-        newGame5.setTitle("New Game - 5 Cards", for: .normal)
     }
     @IBAction func newGame5 (_ sender: UIButton) {
-        gameTitle.isHidden = true
         newGameCard(a: 5)
-        messageLabel.text = "Find the king!"
-        sender.setTitle("Continue!", for: .normal)
-        newGame3.setTitle("New Game - 3 Cards", for: .normal)
-        newGame4.setTitle("New Game - 4 Cards", for: .normal)
     }
     @IBAction func resetScore (_ sender: UIButton) {
         gameTitle.isHidden = false
@@ -253,10 +110,7 @@ class ViewController: UIViewController {
         setFalse()
         setHide()
         messageLabel.text = "Find the king! Choose a game to start!"
-        newGame3.setTitle("New Game - 3 Cards", for: .normal)
-        newGame4.setTitle("New Game - 4 Cards", for: .normal)
-        newGame5.setTitle("New Game - 5 Cards", for: .normal)
-
+        titleChanger(a: 0)
     }
 }
 
