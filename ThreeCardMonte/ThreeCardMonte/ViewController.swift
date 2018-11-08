b08b710dfd46d035972f01fc29618483a3f2b96d
JianTing-Li <jiantingli@pursuit.org>
2018-11-08 01:34:36 -0500

    finifinished bounses, when available beautify the app

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -13,74 +13,75 @@ class ViewController: UIViewController {
     @IBOutlet weak var card1: UIButton!
     @IBOutlet weak var card2: UIButton!
     @IBOutlet weak var card3: UIButton!
+    @IBOutlet weak var card4: UIButton!
+    @IBOutlet weak var card5: UIButton!
+    
     @IBOutlet weak var gameState: UILabel!
-    @IBOutlet weak var newGame: UIButton!
+    @IBOutlet weak var winNum: UILabel!
+    @IBOutlet weak var loseNum: UILabel!
     
-    var cards = [UIButton]()
+    @IBOutlet weak var resetScore: UIButton!
+    @IBOutlet weak var newGame3: UIButton!
+    @IBOutlet weak var newGame4: UIButton!
+    @IBOutlet weak var newGame5: UIButton!
     
-    //card images when flipped
-        //how to safely unwrap this
-    var images = [UIImage.init(named: "threeCard"),
-                  UIImage.init(named: "threeCard"),
-                  UIImage.init(named: "kingCard")]
-    
+    var cardButtons = [UIButton]()
+    var cardImages = [UIImage]()
     let faceDownCard = UIImage.init(named: "cardBackRed")
     
     override func viewDidLoad() { //runs 1 time at the beginning
-    super.viewDidLoad()
-    cards = [card1, card2, card3]
-    images = images.shuffled()
+        super.viewDidLoad()
+        cardButtons = [card1, card2, card3, card4, card5]
+        cardImages = cardImgGenerator(cardNum: 3).shuffled()
+        cardsDisplay(totalCards: 3)
     }
-  
+    
+    var wins = 0
+    var loses = 0
+
     //actions
     @IBAction func flipCard(_ sender: UIButton) {
-        var winOrLose: Bool = true
-        switch sender.tag {
-        case 0:
-            card1.setImage(images[0], for: .normal)
-            winOrLose = winOrLoseFunc(imageOptional: images[0], gameState: gameState)
-            if !winOrLose { flipKingCard(cards: cards, images: images) }
-        case 1:
-            card2.setImage(images[1], for: .normal)
-            winOrLose = winOrLoseFunc(imageOptional: images[1], gameState: gameState)
-            if !winOrLose { flipKingCard(cards: cards, images: images) }
-        case 2:
-            card3.setImage(images[2], for: .normal)
-            winOrLose = winOrLoseFunc(imageOptional: images[2], gameState: gameState)
-            if !winOrLose { flipKingCard(cards: cards, images: images) }
-        default:
-            print("Error")
-        }
+        let cardSelect = sender.tag
+        //flipCard
+        cardButtons[cardSelect].setImage(cardImages[cardSelect], for: .normal)
         
-        buttonControlOnOff(buttons: cards, enabled: false)
+        //Game Result
+        let win = gameResult(image: cardImages[cardSelect], gameState: gameState)
+        
+        //Review King card if the player loses
+        if !win { revealKingCard(cards: cardButtons, images: cardImages) }
+        
+        //update score
+        scoreTrack(result: win)
+        
+        disableOrEnableButtons(buttons: cardButtons, enabled: false)
     }
-    
+
     //Reset Game When "New Game" is tapped
-    @IBAction func resetGame(_ sender: UIButton) {
-        images = images.shuffled()
+    @IBAction func resetGameClick(_ sender: UIButton) {
+        let cardNum = sender.tag
+        
+        cardImages = cardImgGenerator(cardNum: cardNum).shuffled()
         gameState.text = "Pick A Card 🤚🏽"
-        card1.setImage(faceDownCard, for: .normal)
-        card2.setImage(faceDownCard, for: .normal)
-        card3.setImage(faceDownCard, for: .normal)
-        buttonControlOnOff(buttons: cards, enabled: true)
-    }
-    
-    
-    
-    //Functions
-    func buttonControlOnOff(buttons: [UIButton], enabled: Bool) -> () {
-        for button in buttons {
-            if enabled {
-                button.isEnabled = true
-                continue
-            }
-            button.isEnabled = false
+        disableOrEnableButtons(buttons: cardButtons, enabled: true)
+        cardsDisplay(totalCards: cardNum)
+        for card in cardButtons {
+            card.setImage(faceDownCard, for: .normal)
         }
     }
-    
 
-    func winOrLoseFunc(imageOptional: UIImage?, gameState: UILabel) -> Bool {
-        guard let image = imageOptional else {return false}
+
+    @IBAction func resetScore(_ sender: Any) {
+        wins = 0
+        winNum.text = "Wins: \(wins)"
+        
+        loses = 0
+        loseNum.text = "Loses: \(loses)"
+    }
+    
+    
+    //gameplay methods
+    func gameResult(image: UIImage, gameState: UILabel) -> Bool {
         if image == UIImage.init(named: "kingCard") {
             gameState.text = "You Win! 😄"
             return true
@@ -89,13 +90,55 @@ class ViewController: UIViewController {
             return false
         }
     }
-    
-    func flipKingCard(cards: [UIButton], images: [UIImage?]) -> () {
-        for (i, imageOptional) in images.enumerated() {
-            guard let image = imageOptional else {continue}
+
+    func scoreTrack(result: Bool) {
+        if result {
+            wins += 1
+            winNum.text = "Wins: \(wins)"
+        } else {
+            loses += 1
+            loseNum.text = "Loses: \(loses)"
+        }
+    }
+
+    func revealKingCard(cards: [UIButton], images: [UIImage]) {
+        for (i, image) in images.enumerated() {
             if image == UIImage.init(named: "kingCard") {
                 cards[i].setImage(image, for: .normal)
+                break
             }
+        }
+    }
+
+    //Other Functions
+    func disableOrEnableButtons(buttons: [UIButton], enabled: Bool) -> () {
+        for button in buttons {
+            if enabled {
+                button.isEnabled = true
+                continue
+            }
+            button.isEnabled = false
+        }
+    }
+
+    func cardImgGenerator(cardNum: Int) -> [UIImage] {
+        var cardImages = [UIImage.init(named: "kingCard")!]
+        for _ in 1..<cardNum {
+            cardImages.append(UIImage.init(named: "threeCard")!)
+        }
+        return cardImages
+    }
+    
+    func cardsDisplay(totalCards: Int) {
+        if totalCards == 3 {
+            cardButtons[3].isHidden = true
+            cardButtons[4].isHidden = true
+        } else if totalCards == 5 {
+            cardButtons[3].isHidden = false
+            cardButtons[4].isHidden = false
+        } else if totalCards == 4 {
+            cardButtons[3].isHidden = false
+            cardButtons[4].isHidden = true
         }
     }
 }
