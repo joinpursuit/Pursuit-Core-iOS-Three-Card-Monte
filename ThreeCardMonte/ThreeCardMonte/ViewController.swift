c22c3ccb8cf138f14d2c94ef0fe58d03658bf492
Aaron Pachesa <aaronpachesa@Aarons-MacBook-Pro-2.local>
2019-07-30 10:16:51 -0400

    pushing the latest copy

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -8,13 +8,110 @@
 
 import UIKit
 
+var arrays = ["right", "left", "bottom"]
+var winningCard = arrays.randomElement()
+let kingImage = UIImage(named: "kingCard")
+let threeImage = UIImage(named: "threeCard")
+let backImage = UIImage(named: "cardBackRed")
+
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
+    
+    @IBOutlet weak var cardLeft: UIButton!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var cardBottom: UIButton!
+    @IBOutlet weak var cardRight: UIButton!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+ 
+    
+    
+    
+    
+    @IBAction func cardOnRightTouch(_ sender: UIButton) {
+        if winningCard == "right" {
+            self.textLabel.text = "you won"
+            self.cardLeft.isEnabled = false
+            self.cardBottom.isEnabled = false
+            self.cardRight.setImage(kingImage, for: UIControl.State.normal)
+        } else if winningCard == "left" {
+            self.textLabel.text = "you lost"
+            self.cardLeft.isEnabled = false
+            self.cardBottom.isEnabled = false
+            self.cardRight.setImage(threeImage, for: UIControl.State.normal)
+            self.cardBottom.setImage(kingImage, for: UIControl.State.normal)
+        } else if winningCard == "bottom" {
+            self.textLabel.text = "you lost"
+            self.cardLeft.isEnabled = false
+            self.cardBottom.isEnabled = false
+            self.cardRight.setImage(threeImage, for: UIControl.State.normal)
+            self.cardLeft.setImage(kingImage, for: UIControl.State.normal)
+        }
+    }
+    
+    
+    
+    @IBAction func cardOnLeftTouch(_ sender: UIButton) {
+        if winningCard == "left" {
+            self.textLabel.text = "you won"
+            self.cardBottom.isEnabled = false
+            self.cardRight.isEnabled = false
+            self.cardLeft.setImage(kingImage, for: UIControl.State.normal)
+        } else if winningCard == "right" {
+            self.textLabel.text = "you lost"
+            self.cardBottom.isEnabled = false
+            self.cardRight.isEnabled = false
+            self.cardLeft.setImage(threeImage, for: UIControl.State.normal)
+            self.cardRight.setImage(kingImage, for: UIControl.State.normal)
+        } else if winningCard == "bottom" {
+            self.textLabel.text = "you lost"
+            self.cardBottom.isEnabled = false
+            self.cardRight.isEnabled = false
+            self.cardLeft.setImage(threeImage, for: UIControl.State.normal)
+            self.cardBottom.setImage(kingImage, for: UIControl.State.normal)
+        }
+      
+    }
+    
+    
+    
+    @IBAction func cardOnBottomTouch(_ sender: UIButton) {
+        if winningCard == "bottom" {
+            self.textLabel.text = "you won"
+            self.cardLeft.isEnabled = false
+            self.cardRight.isEnabled = false
+            self.cardBottom.setImage(kingImage, for: UIControl.State.normal)
+        } else if winningCard == "right" {
+            self.textLabel.text = "you lost"
+            self.cardLeft.isEnabled = false
+            self.cardRight.isEnabled = false
+            self.cardBottom.setImage(threeImage, for: UIControl.State.normal)
+            self.cardLeft.setImage(kingImage, for: UIControl.State.normal)
+        } else if winningCard == "left" {
+            self.textLabel.text = "you lost"
+            self.cardLeft.isEnabled = false
+            self.cardRight.isEnabled = false
+            self.cardBottom.setImage(threeImage, for: UIControl.State.normal)
+            self.cardRight.setImage(kingImage, for: UIControl.State.normal)
+        }
+        
+    }
+    
+    
+    
+    @IBAction func newGameTouch(_ sender: Any) {
+        self.cardLeft.isEnabled = true
+        self.cardBottom.isEnabled = true
+        self.cardRight.isEnabled = true
+        self.textLabel.text = "Pick a card, any card!"
+        winningCard = arrays.randomElement()
+        self.cardBottom.setImage(backImage, for: UIControl.State.normal)
+        self.cardRight.setImage(backImage, for: UIControl.State.normal)
+        self.cardLeft.setImage(backImage, for: UIControl.State.normal)
+        
+    }
 
 }
 
