77fab52d6c381b813721f01630a8234148519756
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-10-28 17:16:21 -0400

    Title, card prompt, cardback images/buttons, changes prompt to win message if picks 3rd card

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    @IBOutlet weak var cardSelectLabel: UILabel!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-  }
+    }
 
 
+    
+    @IBAction func pickCard(_ sender: UIButton) {
+        
+        let cardButton = UIButton()
+        
+        switch  sender.tag {
+            
+        case 0:
+            cardSelectLabel.text = "You Lose!"
+        case 1:
+            cardSelectLabel.text = "You Lose!"
+        case 2:
+            cardSelectLabel.text = "You Win!"
+            cardButton.setBackgroundImage(UIImage: "kingCard", for: <#UIControl.State#>)
+        default:
+            cardSelectLabel.text = "Game Error!"
+        }
+    
+    }
+    
 }
 
