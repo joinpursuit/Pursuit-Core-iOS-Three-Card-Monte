ce70816620f02cf2c419c48ddf29607bd0de5db1
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-10-28 14:01:22 -0400

    Created three buttons, allows for win/lose.  New game button added.  Display text added.

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBOutlet weak var cardOne: UIButton!
+    @IBOutlet weak var cardTwo: UIButton!
+    @IBOutlet weak var cardThree: UIButton!
+    
+    lazy var cards: [UIButton] = [cardOne, cardTwo, cardThree]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func chooseCard(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            displayLabel.text = "You lost!"
+        case 1:
+            displayLabel.text = "You lost!"
+        case 2:
+            displayLabel.text = "You won!"
+        default:
+            displayLabel.text = "What did you do?!"
+        }
+        for card in cards {
+            card.isEnabled = false
+        }
+    }
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        displayLabel.text = "Pick a card"
+        for card in cards {
+            card.isEnabled = true
+        }
+    }
+    
 }
 
