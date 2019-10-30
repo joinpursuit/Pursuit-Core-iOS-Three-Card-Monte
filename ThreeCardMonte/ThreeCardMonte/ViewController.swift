023e2fe9cf00701aa3338e6761e857c21de0f747
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-10-29 21:21:05 -0400

    Cleaned up, adding new game option

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-let typeCardArray = ["cardBackRed","threeCard","kingCard"]
+var typeCardArray = ["cardBackRed","threeCard","kingCard"]
 var cardsArrayIfLost = ["cardBackRed","kingCard"]
 
 let cardBackRed = typeCardArray[0]
@@ -17,78 +17,94 @@ let kingCard = typeCardArray[2]
 var randomCard = typeCardArray.randomElement()
 var shuffled = cardsArrayIfLost.shuffled()
 
+
 class ViewController: UIViewController {
    
     @IBOutlet weak var card1: UIButton!
     @IBOutlet weak var card2: UIButton!
     @IBOutlet weak var card3: UIButton!
+    @IBOutlet weak var newGame: UIButton!
     
     @IBOutlet weak var cardSelectLabel: UILabel!
-   
-    override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    let selected = UIControl.State.normal
+    let cardImage = UIImage(named: threeCard )
+    let cardImage2 = UIImage(named: kingCard)
+    let randomImage = UIImage(named: shuffled[0])
+    let randomImage2 = UIImage(named: shuffled[1])
+    let defaultImage = UIImage(named: cardBackRed)
+    
+    func reset()    {
+        typeCardArray = ["cardBackRed","threeCard","kingCard"]
+        cardsArrayIfLost = ["cardBackRed","kingCard"]
+        card1.isEnabled = true
+        card2.isEnabled = true
+        card3.isEnabled = true
+        card1.setBackgroundImage(defaultImage, for: selected)
+        card2.setBackgroundImage(defaultImage, for: selected)
+        card3.setBackgroundImage(defaultImage, for: selected)
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        }
 
+    
+    
     @IBAction func pickCard(_ sender: UIButton) {
-        
-        
-        let selected = UIControl.State.normal
-        let selected2 = UIControl.State.disabled
-        let cardImage = UIImage(named: threeCard )
-        let cardImage2 = UIImage(named: kingCard)
-        let randomImage = UIImage(named: shuffled[0])
-        let randomImage2 = UIImage(named: shuffled[1])
-        
+    
         switch  sender.tag {
-        case 0:
-            if randomCard != kingCard   {
-                cardSelectLabel.text = "You Lose!"
-                sender.setBackgroundImage(cardImage, for: selected)
-               
-                card2.setBackgroundImage(randomImage, for: selected)
-                
-                card3.setBackgroundImage(randomImage2, for: selected)
-            }
-            else    {
-                cardSelectLabel.text = "You Win!"
+            case 0:
+                if randomCard != kingCard   {
+                    cardSelectLabel.text = "You Lose!"
+                    sender.setBackgroundImage(cardImage, for: selected)
+                          
+                    card2.setBackgroundImage(randomImage, for: selected)
+                           
+                    card3.setBackgroundImage(randomImage2, for: selected)
+                       }
+                else    {
+                    cardSelectLabel.text = "You Win!"
                 sender.setBackgroundImage(cardImage2, for: selected)
-            }
+                       }
         case 1:
             if randomCard != kingCard   {
+                
                 cardSelectLabel.text = "You Lose!"
                 sender.setBackgroundImage(cardImage, for: selected)
-               
+                          
                 card1.setBackgroundImage(randomImage, for: selected)
-                
+                           
                 card3.setBackgroundImage(randomImage2, for: selected)
-                card1.isEnabled = false
-                card2.isEnabled = false
-                card3.isEnabled = false
-                
-            }
+                       }
             else    {
                 cardSelectLabel.text = "You Win!"
                 sender.setBackgroundImage(cardImage2, for: selected)
-                card1.isEnabled = false
-                card3.isEnabled = false
-            }
+                       }
         case 2:
             if randomCard != kingCard   {
                 cardSelectLabel.text = "You Lose!"
-                sender.setBackgroundImage(cardImage, for: selected)
-               
-                card1.setBackgroundImage(randomImage, for: selected)
                 
+                sender.setBackgroundImage(cardImage, for: selected)
+                          
+                card1.setBackgroundImage(randomImage, for: selected)
+                           
                 card2.setBackgroundImage(randomImage2, for: selected)
-            }
+                       }
             else    {
                 cardSelectLabel.text = "You Win!"
                 sender.setBackgroundImage(cardImage2, for: selected)
-            }
+                       }
+        case 3:
+                       
+            print("look at me")
+        
         default:
-            cardSelectLabel.text = "Game Error!"
+            print("look at me")
         }
+    card1.isEnabled = false
+    card2.isEnabled = false
+    card3.isEnabled = false
     }
 }
-
