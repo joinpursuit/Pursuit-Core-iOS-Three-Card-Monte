a967b20c726860c7856bd85b7784e2b432e819e9
Yuliia Engman <yuliia.engman@gmail.com>
2019-10-30 13:20:59 -0400

    completed game

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -23,67 +23,87 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    labelAboutGameStatus.text = "Pick any card"
+        labelAboutGameStatus.text = "Pick any card"
     }
     
     @IBAction func cardGame(_ sender: UIButton) {
-        
-//        winningcard = rangeOfCards.randomElement()
-        //print(winningcard)
         
         switch sender.tag {
         case 0:
             if sender.tag == winningcard {
                 labelAboutGameStatus.text = "You win!"
                 sender.setImage(UIImage(named: "kingCard"), for: .normal)
-                card1.isEnabled = false //why false?
+                card1.isEnabled = false
                 card2.isEnabled = false
                 card3.isEnabled = false
                 
-
             } else {
                 labelAboutGameStatus.text = "You lose!"
                 sender.setImage(UIImage(named: "threeCard"), for: .normal)
-                card1.isEnabled = false //why false?
+                winningCardOpens()
+                card1.isEnabled = false
                 card2.isEnabled = false
                 card3.isEnabled = false
             }
-
-            
-            
-//        sender.setImage(UIImage(named: "threeCard"), for: .normal)
-           // sender.isEnabled
             
         case 1:
-            labelAboutGameStatus.text = "You win!"
-            
-            sender.setImage(UIImage(named: "kingCard"), for: .normal)
+            if sender.tag == winningcard {
+                labelAboutGameStatus.text = "You win!"
+                sender.setImage(UIImage(named: "kingCard"), for: .normal)
+                card1.isEnabled = false
+                card2.isEnabled = false
+                card3.isEnabled = false
+                
+            } else {
+                labelAboutGameStatus.text = "You lose!"
+                sender.setImage(UIImage(named: "threeCard"), for: .normal)
+                winningCardOpens()
+                card1.isEnabled = false
+                card2.isEnabled = false
+                card3.isEnabled = false
+            }
             
         default:
-            labelAboutGameStatus.text = "You lose!"
-            
-            sender.setImage(UIImage(named: "threeCard"), for: .normal)
-        }
+            if sender.tag == winningcard {
+                labelAboutGameStatus.text = "You win!"
+                sender.setImage(UIImage(named: "kingCard"), for: .normal)
+                card1.isEnabled = false
+                card2.isEnabled = false
+                card3.isEnabled = false
                 
+            } else {
+                labelAboutGameStatus.text = "You lose!"
+                sender.setImage(UIImage(named: "threeCard"), for: .normal)
+                winningCardOpens()
+                card1.isEnabled = false
+                card2.isEnabled = false
+                card3.isEnabled = false
+            }
+        }
     }
     
     @IBAction func resetGame(_ sender: UIButton) {
-        
         card1.setImage(UIImage(named: "cardBackRed"), for: .normal)
         card2.setImage(UIImage(named: "cardBackRed"), for: .normal)
         card3.setImage(UIImage(named: "cardBackRed"), for: .normal)
         winningcard = Int.random(in: 0...2)
-        print(winningcard)
         labelAboutGameStatus.text = "Pick any card"
-        card1.isEnabled = true//why false?
+        card1.isEnabled = true
         card2.isEnabled = true
         card3.isEnabled = true
     }
+    
+    func winningCardOpens() {
+        if winningcard == 0 {
+            card1.setImage(UIImage(named: "kingCard"), for: .normal)
+        } else if winningcard == 1 {
+            card2.setImage(UIImage(named: "kingCard"), for: .normal)
+        } else {
+            card3.setImage(UIImage(named: "kingCard"), for: .normal)
+        }
+    }
 }
-    //    @IBAction func newGame(_ sender: UIButton) {
-//        sender.tag
-//        sender.setImage("cardBackRed", for: .normal)
-//    }
+
+
 
 
